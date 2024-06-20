018c36d2352e432b4fe6778e37b292a8e6ccc0e7
VeberAL <aleo83@rambler.ru>
2024-06-21 02:14:09 +0500

    добавить owner конфиги

---
+++ src/test/java/config/WebConfig.java
@@ -0,0 +1,32 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "classpath:${env}.properties"
+})
+public interface WebConfig extends Config {
+    @Key("baseUrl")
+    @DefaultValue("https://music.youtube.com")
+    String getBaseUrl();
+
+    @Key("browser")
+    @DefaultValue("CHROME")
+    Browser getBrowser();
+
+    @Key("browserSize")
+    @DefaultValue("1920x1080")
+    String getBrowserSize();
+
+    @Key("browserVersion")
+    @DefaultValue("100.0")
+    String getBrowserVersion();
+
+    @Key("isRemote")
+    @DefaultValue("false")
+    Boolean isRemote();
+
+    @Key("remoteUrl")
+    String getRemoteUrl();
+}
